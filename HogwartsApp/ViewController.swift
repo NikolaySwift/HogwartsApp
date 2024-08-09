952532e49686fbc87e97cd27174bfe0a1390b0e0
NikolayD <volodar86@yandex.ru>
2024-08-09 15:28:38 +0300

    Initial Commit

---
+++ HogwartsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HogwartsApp
+//
+//  Created by NikolayD on 09.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
