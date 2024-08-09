c262784ebc1c3683ffe70ea3e8c5347e07d0690f
NikolayD <volodar86@yandex.ru>
2024-08-09 17:30:12 +0300

    create datastore

--- HogwartsApp/Services/DataStore.swift
+++ HogwartsApp/Services/DataStore.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+enum House: String, CaseIterable {
+    case gryffindor = "Griffindor"
+    case slytherin = "Slytherin"
+    case hufflepuff = "Hufflepuff"
+    case ravenclaw = "Ravenclaw"
+    
+    var url: URL {
+        switch self {
+        case .gryffindor:
+            return URL(string: "https://hp-api.herokuapp.com/api/characters/house/gryffindor")!
+        case .slytherin:
+            return URL(string: "https://hp-api.herokuapp.com/api/characters/house/slytherin")!
+        case .hufflepuff:
+            return URL(string: "https://hp-api.herokuapp.com/api/characters/house/hufflepuff")!
+        case .ravenclaw:
+            return URL(string: "https://hp-api.herokuapp.com/api/characters/house/ravenclaw")!
+        }
+    }
+}
+
+final class DataStore {
+    static let shared = DataStore()
+    
+    var categories: [House] {
+        House.allCases
+    }
+    
+    private init() {}
+}
