d21cf990fdbdf87ee4f6877c0787a203d729fd73
NikolayD <volodar86@yandex.ru>
2024-08-09 17:29:25 +0300

    create model of character from hogwarts

--- HogwartsApp/Models/Character.swift
+++ HogwartsApp/Models/Character.swift
@@ -5,4 +5,25 @@
 //  Created by NikolayD on 09.08.2024.
 //
 
-import Foundation
+
+struct HogwartsCharacter: Decodable {
+    let name: String
+    let alternate_names: [String]
+    let species: String
+    let gender: String
+    let house: String
+    let dateOfBirth: String?
+    let yearOfBirth: Int?
+    let wand: Wand
+    let ancestry: String
+    let patronus: String
+    let actor: String
+    let image: String
+}
+
+struct Wand: Decodable {
+    let wood: String
+    let core: String
+    let length: Double?
+}
+
