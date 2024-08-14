80a67f47e2f9951007ce78bf3627c74b33ea783f
NikolayD <volodar86@yandex.ru>
2024-08-14 12:20:59 +0300

    override method to show list of characters. define method to fetch characters from server

--- HogwartsApp/ViewControllers/CharactersListViewController.swift
+++ HogwartsApp/ViewControllers/CharactersListViewController.swift
@@ -7,83 +7,55 @@
 
 import UIKit
 
-class CharactersListViewController: UITableViewController {
+final class CharactersListViewController: UITableViewController {
+    
+    var houseName: String!
+    
+    private let networkManager = NetworkManager.shared
+    private var characters: [HogwartsCharacter] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.rowHeight = 100
+        
+        navigationItem.title = houseName
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let detailsVC = segue.destination as? DetailsViewController else { return }
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        
+        detailsVC.character = characters[indexPath.row]
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    // MARK: - TableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        characters.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "characterCell", for: indexPath)
+        guard let cell = cell as? CharacterCell else { return UITableViewCell() }
+        
+        cell.configure(with: characters[indexPath.row], and: tableView.rowHeight)
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func fetchCharacters(from house: House) {
+        networkManager.fetch(
+            [HogwartsCharacter].self,
+            from: house.url) { [weak self] result in
+                guard let self else { return }
+                switch result {
+                case .success(let characters):
+                    self.characters = characters.filter{ !$0.image.isEmpty }
+                    tableView.reloadData()
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
