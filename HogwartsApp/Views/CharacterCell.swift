1acbf7db176786bbcd96afa00251611c89f835fa
NikolayD <volodar86@yandex.ru>
2024-08-14 12:15:08 +0300

    for table cell define custom cell class with two labels and image that will be fetch from server

--- HogwartsApp/Views/CharacterCell.swift
+++ HogwartsApp/Views/CharacterCell.swift
@@ -5,4 +5,30 @@
 //  Created by NikolayD on 13.08.2024.
 //
 
-import Foundation
+import UIKit
+
+final class CharacterCell: UITableViewCell {
+    
+    @IBOutlet var characterImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var actorLabel: UILabel!
+    
+    private let networkManager = NetworkManager.shared
+    
+    func configure(with character: HogwartsCharacter, and rowHeight: CGFloat) {
+        nameLabel.text = character.name
+        actorLabel.text = character.actor
+        
+        characterImageView.layer.cornerRadius = rowHeight / 2
+        
+        networkManager.fetchImage(from: URL(string: character.image)!) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let imageData):
+                characterImageView.image = UIImage(data: imageData)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
