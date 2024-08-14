46992a161db2775636685ad49012df84332dd45a
NikolayD <volodar86@yandex.ru>
2024-08-14 12:11:55 +0300

    change name property style. add description of character and wand

--- HogwartsApp/Models/Character.swift
+++ HogwartsApp/Models/Character.swift
@@ -8,22 +8,51 @@
 
 struct HogwartsCharacter: Decodable {
     let name: String
-    let alternate_names: [String]
+    let alternateNames: [String]
     let species: String
     let gender: String
-    let house: String
     let dateOfBirth: String?
-    let yearOfBirth: Int?
     let wand: Wand
-    let ancestry: String
     let patronus: String
     let actor: String
     let image: String
+    let hogwartsStudent: Bool
+    let hogwartsStaff: Bool
+    
+    var description: String {
+        """
+        Species: \(species)
+        Gender: \(gender)
+        Date of birth: \(dateOfBirth ?? "unknown")
+        Patronus: \(patronus.isEmpty ? "unknown" : patronus)
+        Alternate names: \(alternateNames.isEmpty ? "none" : alternateNames.formatted())
+        Actor: \(actor)
+        
+        \(wand.description)
+        """
+    }
 }
 
 struct Wand: Decodable {
     let wood: String
     let core: String
     let length: Double?
+    
+    var description: String {
+        """
+        Wand was made from:
+            - wood: \(wood.isEmpty ? "unknown" : wood)
+            - core: \(core.isEmpty ? "unknown" : core).
+        and its length: \(lengthToString)
+        """
+    }
+    
+    private var lengthToString: String {
+        if let length {
+            return String(format: "%.2f inches", length)
+        } else {
+            return "unknown"
+        }
+    }
 }
 
