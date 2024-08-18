0fa5cc7ae10b929ba451eee77373915715cb7804
NikolayD <volodar86@yandex.ru>
2024-08-18 18:52:41 +0300

    add new file with network manager with using of framework alamofire. make two methods to fetch array of characters and data

--- HogwartsApp/Services/AlamofireNetworkManager.swift
+++ HogwartsApp/Services/AlamofireNetworkManager.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import Alamofire
+
+final class AlamofireNetworkManager {
+    static let shared = AlamofireNetworkManager()
+    
+    func fetchCharacters(
+        from url: URL,
+        completion: @escaping(Result<[HogwartsCharacter], AFError>) -> Void
+    ) {
+        AF.request(url)
+            .validate()
+            .responseJSON { dataResponse in
+                switch dataResponse.result {
+                case .success(let value):
+                    let characters: [HogwartsCharacter] = HogwartsCharacter.getCharacters(from: value)
+                    completion(.success(characters))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+    
+    func fetchData(
+        from url: String,
+        completion: @escaping(Result<Data, AFError>) -> Void
+    ){
+        AF.request(url)
+            .validate()
+            .responseData { dataResponse in
+                switch dataResponse.result {
+                case .success(let data):
+                    completion(.success(data))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+    
+    private init() {}
+}
